80e700ba7897686d6355db7588d8f657a6c78828
ohluwa <famuyibo.tim@gmail.com>
2022-03-29 02:10:00 -0700

    Edit number_guesser.py

--- py_guess_number/number_guesser.py
+++ py_guess_number/number_guesser.py
@@ -1,5 +1,6 @@
 import random
 
+#Function To Initiate Game
 def guesser(num):
     j = 0
     while (j < 1):
@@ -7,7 +8,7 @@ def guesser(num):
         if (user_guess == randnum):
             print("Congratulations, you guessed the correct number!")
             guessednums.append(user_guess)
-            print(guessednums)
+            print("Here are the numbers that you have guessed:\n" + str(guessednums))
             j+= 1
             break
         if (user_guess > randnum):
@@ -20,6 +21,7 @@ def guesser(num):
             guesser(int(input()))
         return j
 
+#Function To Allow User To Restart Game
 def restart(response):
     i = 0
     while(i < 1):
@@ -32,15 +34,28 @@ def restart(response):
             exit()
         else:
             print("ERROR: Please enter Y or N.")
-            restart(str(input()))
+            restart(str(input().lower()))
         return response
 
+#To Prevent Invalid Errors From Crashing Program
+def handle_execption():
+    while True:
+        try:
+            guesser(int(input()))
+            break
+        except ValueError:
+            print("That is not a valid input. Try again.")
+            handle_execption()
+    return False
+
+#Main Loop
 i = 0
 while (i < 1):
     randnum = random.randint(1,101)
-    print(randnum)
+    #Can Be Uncommented To See Random Generated Number
+    #print(randnum)
     guessednums = []
     print("Please guess a number between 1 and 100.: ")
-    guesser(int(input()))
+    handle_execption()
     print("Would you like to play again? Y/N")
     restart(str(input().lower()))
